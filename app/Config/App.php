f8e4961d07d709c622258af618946914a226c2a8
Fmaciel13 <fatimamaciel897@gmail.com>
2025-08-26 20:56:47 -0300

    primer commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/universidad/public/';
+    public string $baseURL = 'http://localhost/appuni/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
